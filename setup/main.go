264ddfd9e56807bee32820f18e05a829fbfbc0f8
TrapLord92 <traplord345@gmail.com>
2024-09-18 01:35:50 +0500

    extrating parameters from the request url

--- setup/main.go
+++ setup/main.go
@@ -4,14 +4,15 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	router := gin.Default()
-	router.GET("/", CustomHandler)
+	router.GET("/:name", CustomHandler)
 
 	router.Run()
 }
 
 // custum handler
 func CustomHandler(c *gin.Context) {
+	name := c.Params.ByName("name")
 	c.JSON(200, gin.H{
-		"message": "setup done you are ready to go",
+		"message": "Hello, " + name + "! Welcome to my API!",
 	})
 }
