07a2994fa75b85061efbe1c93afd0aab01a43daa
TrapLord92 <traplord345@gmail.com>
2024-09-18 01:10:56 +0500

    Custom handler

--- setup/main.go
+++ setup/main.go
@@ -4,10 +4,14 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	router := gin.Default()
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "setup done you are ready to go",
-		})
-	})
+	router.GET("/", CustomHandler)
+
 	router.Run()
+}
+
+// custum handler
+func CustomHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "setup done you are ready to go",
+	})
 }
