891cd845f55b92c6afbb86505a3c278ebc948961
TrapLord92 <traplord345@gmail.com>
2024-09-20 19:28:28 +0500

    session based authentication

---
+++ S3Building-API-Authentication/Api-Autentication-cookies/handlers/auth.go
@@ -0,0 +1,141 @@
+package handlers
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"net/http"
+	"time"
+
+	"github.com/TrapLord92/Building-Distributed-Applications-With-Golang/S3Building-API-Authentication/Api-Autentication-cookies/models"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-gonic/gin"
+
+	"github.com/rs/xid"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/net/context"
+)
+
+type AuthHandler struct {
+	collection *mongo.Collection
+	ctx        context.Context
+}
+
+type Claims struct {
+	Username string `json:"username"`
+	jwt.StandardClaims
+}
+
+type JWTOutput struct {
+	Token   string    `json:"token"`
+	Expires time.Time `json:"expires"`
+}
+
+func NewAuthHandler(ctx context.Context, collection *mongo.Collection) *AuthHandler {
+	return &AuthHandler{
+		collection: collection,
+		ctx:        ctx,
+	}
+}
+
+// swagger:operation POST /signin auth signIn
+// Login with username and password
+// ---
+// produces:
+// - application/json
+// responses:
+//
+//	'200':
+//	    description: Successful operation
+//	'401':
+//	    description: Invalid credentials
+func (handler *AuthHandler) SignInHandler(c *gin.Context) {
+	var user models.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Hash the user's password
+	h := sha256.New()
+	h.Write([]byte(user.Password))
+	hashedPassword := hex.EncodeToString(h.Sum(nil)) // Convert the hash to a hex string
+
+	// Find user in MongoDB with hashed password
+	cur := handler.collection.FindOne(handler.ctx, bson.M{
+		"username": user.Username,
+		"password": hashedPassword,
+	})
+	if cur.Err() != nil {
+		// Invalid credentials
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid username or password"})
+		return
+	}
+
+	// If user is found, create a session
+	sessionToken := xid.New().String()
+	session := sessions.Default(c)
+	session.Set("username", user.Username)
+	session.Set("token", sessionToken)
+	session.Save()
+
+	// Return successful login response
+	c.JSON(http.StatusOK, gin.H{"message": "User signed in"})
+}
+
+// swagger:operation POST /refresh auth refresh
+// Refresh token
+// ---
+// produces:
+// - application/json
+// responses:
+//
+//	'200':
+//	    description: Successful operation
+//	'401':
+//	    description: Invalid credentials
+func (handler *AuthHandler) RefreshHandler(c *gin.Context) {
+	session := sessions.Default(c)
+	sessionToken := session.Get("token")
+	sessionUser := session.Get("username")
+	if sessionToken == nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid session cookie"})
+		return
+	}
+
+	sessionToken = xid.New().String()
+	session.Set("username", sessionUser.(string))
+	session.Set("token", sessionToken)
+	session.Save()
+
+	c.JSON(http.StatusOK, gin.H{"message": "New session issued"})
+}
+
+// swagger:operation POST /signout auth signOut
+// Signing out
+// ---
+// responses:
+//
+//	'200':
+//	    description: Successful operation
+func (handler *AuthHandler) SignOutHandler(c *gin.Context) {
+	session := sessions.Default(c)
+	session.Clear()
+	session.Save()
+	c.JSON(http.StatusOK, gin.H{"message": "Signed out..."})
+}
+
+func (handler *AuthHandler) AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		session := sessions.Default(c)
+		sessionToken := session.Get("token")
+		if sessionToken == nil {
+			c.JSON(http.StatusForbidden, gin.H{
+				"message": "Not logged",
+			})
+			c.Abort()
+		}
+		c.Next()
+	}
+}
